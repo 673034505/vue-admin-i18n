02d543f0156609b3954825ae08f872c117b60f84
LRR <673034505@qq.com>
2021-06-06 16:22:25 +0800

    [添加]:新增页面组件

---
+++ src/layout/components/Sidebar/index.vue
@@ -0,0 +1,41 @@
+<template>
+  <el-scrollbar wrap-class="scrollbar-wrapper">
+    <logo :is-collapse="isCollapse" />
+    <el-menu
+      class="height"
+      mode="vertical"
+      :show-timeout="200"
+      :default-active="$route.path"
+      :collapse="isCollapse"
+      :unique-opened="true"
+      background-color="#6959CD"
+      text-color="white"
+      active-text-color="#42b983"
+    >
+      <sidebar-item :routes="permission_routers" />
+    </el-menu>
+  </el-scrollbar>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+import SidebarItem from './SidebarItem'
+import logo from './logo'
+export default {
+  components: { SidebarItem, logo },
+  computed: {
+    ...mapGetters([
+      'sidebar',
+      'permission_routers'
+    ]),
+    isCollapse() {
+      return !this.sidebar.opened
+    }
+  }
+}
+</script>
+<style lang="scss">
+.height{
+  height: calc(100% - 64px) !important;
+}
+</style>
